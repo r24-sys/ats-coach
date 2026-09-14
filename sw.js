1474fbab77ec274ee2afc1c6a40c56565f7cacc7
r24-sys <ryandufaure@gmail.com>
2026-09-14 21:52:40 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'ats-coach-v3-1-pwa';
-const ASSETS = ['./', './index.html', './manifest.webmanifest', './icons/icon-192.svg', './icons/icon-512.svg'];
+const CACHE = 'ats-coach-v3-pwa-v2';
+const ASSETS = ['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
@@ -10,7 +10,7 @@ self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
   event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
     const copy = response.clone();
-    caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    caches.open(CACHE).then(cache => cache.put(event.request, copy)).catch(() => {});
     return response;
   }).catch(() => caches.match('./index.html'))));
 });
